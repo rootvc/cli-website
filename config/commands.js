2443e9b517852d7820ceddeca897dcaaa820a05c
Lee Edwards <lee@root.vc>
2021-02-11 12:52:16 -0800

    organize css and js

---
+++ config/commands.js
@@ -0,0 +1,15 @@
+const commands = {
+  help: function() {
+    const maxCmdLength = 12;
+    Object.entries(help).forEach(function(kv) {
+      var cmd = kv[0];
+      const rightPad = maxCmdLength - cmd.length;
+      cmd = cmd.concat(" ".repeat(rightPad));
+      term.writeln(cmd + kv[1]);
+    });
+  },
+
+  home: function() {
+    openURL("https://root.vc");
+  }
+}
---
+++ config/help.js
@@ -0,0 +1,4 @@
+const help = {
+  help: "list all commands",
+  home: "visit the Root homepage"
+};
