47f9e6224e4f5e9d65381449324abd95872bf0cd
Avidan Ross <avidan@avidanross.com>
2021-02-19 15:12:46 -0800

    Update help.js

--- config/help.js
+++ config/help.js
@@ -5,7 +5,7 @@ const help = {
   deck: "our fund III deck\r\n",
   "whois [user]": "display info on user. examples:\r\nwhois avidan\r\nwhois kane\r\nwhois chrissy\r\nwhois lee\r\nwhois emily\r\nwhois ian\r\n",
   "dataroom [name]": "access the dataroom section by name, examples:\r\ndataroom main\r\ndataroom memo\r\ndataroom founders\r\ndataroom legal\r\ndataroom social\r\ndataroom team\r\n",
-  "linkedin [user]": "display linkedin info on user. examples:\r\nlinkedin avidan\r\nlinkedin kane\r\nlinkedin chrissy\r\nlinkedin lee\r\nlinkedin emily\r\linkedin ian\r\n",
+  "linkedin [user]": "display linkedin info on user",
   test: "do not use",
   exit: "get out of here and go to the dataroom",
   other: "or try some of your favorite linux commands"
