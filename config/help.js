e91e7c35d6b919fa598ef01448dffe3877b68eab
Avidan Ross <avidan@avidanross.com>
2021-02-19 15:16:59 -0800

    Update help.js

--- config/help.js
+++ config/help.js
@@ -3,10 +3,11 @@ const help = {
   home: "visit the Root homepage",
   email: "email us",
   deck: "our fund III deck\r\n",
-  "whois [user]": "display info on user. examples:\r\nwhois avidan\r\nwhois kane\r\nwhois chrissy\r\nwhois lee\r\nwhois emily\r\nwhois ian\r\n",
-  "dataroom [name]": "access the dataroom section by name, examples:\r\ndataroom main\r\ndataroom memo\r\ndataroom founders\r\ndataroom legal\r\ndataroom social\r\ndataroom team\r\n",
-  "linkedin [user]": "display linkedin info on user",
+  "whois [partner]": "display info on partner",
+  "dataroom [section]": "access the dataroom section by name, examples:\r\ndataroom main\r\ndataroom memo\r\ndataroom founders\r\ndataroom legal\r\ndataroom social\r\ndataroom team\r\n",
+  "linkedin [partner]": "display linkedin info on partner",
   test: "do not use",
+  commit: "go time",
   exit: "get out of here and go to the dataroom",
   other: "or try some of your favorite linux commands"
 };
