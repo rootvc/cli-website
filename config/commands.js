b95db1e115f72db614e579be3a6ef9cae853352a
Lee Edwards <lee@root.vc>
2021-02-19 00:10:38 -0800

    Update help with whois

--- config/commands.js
+++ config/commands.js
@@ -1,6 +1,6 @@
 const commands = {
   help: function() {
-    const maxCmdLength = 12;
+    const maxCmdLength = 16;
     Object.entries(help).forEach(function(kv) {
       var cmd = kv[0];
       const rightPad = maxCmdLength - cmd.length;
--- config/help.js
+++ config/help.js
@@ -1,4 +1,5 @@
 const help = {
   help: "list all commands",
-  home: "visit the Root homepage"
+  home: "visit the Root homepage",
+  "whois [user]": "view a user's info. try: whois avidan"
 };
