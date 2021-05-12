ed0ec7b93c2c70a48afa68ce299be0950f679e12
Kane Hsieh <peepeepoo@computah.local>
2021-05-12 14:40:58 -0700

    copy

--- config/help.js
+++ config/help.js
@@ -1,11 +1,11 @@
 const help = {
   help: "list all commands (you're looking at it)",
-  home: "visit the Root homepage",
   "whois root": "learn about us",
   "whois [partner]": "display info on partner",
   email: "email us",
   twitter: "@machinepix twitter",
   instagram: "@machinepix instagram",
-  test: "do not use",
+  git: "Root github",
+  test: "test command do not use",
   other: "or try some of your favorite linux commands"
 };
