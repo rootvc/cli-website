3ba556df23c48631211c86b0d109393085c8e0b1
emilyhenriksson <79349355+emilyhenriksson@users.noreply.github.com>
2021-06-02 14:27:51 -0700

    Update portfolio.js
    Added missing info to portfolio.js

--- config/portfolio.js
+++ config/portfolio.js
@@ -15,27 +15,52 @@ const portfolio = {
     demo: "https://meroxa.com/#data-warehouse",
   },
 
-  // particle: {
-  //   demo: "https://docs.particle.io/quickstart/boron/",
-  // },
+   particle: {
+     name: "Particle",
+     url: "https://www.particle.io",
+     description: "Lorem ipsum dolor sit amet etc etc",
+     demo: "https://docs.particle.io/quickstart/boron/",
+   },
 
-  // daily: {
-  //   demo: "https://docs.daily.co/docs/introduction-1",
-  // },
+   daily: {
+     name: "Daily",
+     url: "https://www.daily.co",
+     description: "Lorem ipsum dolor sit amet etc etc",
+     demo: "https://docs.daily.co/docs/introduction-1",
+   },
 
-  // hash: {
-  //   demo: "https://core.hash.ai/new",
-  // },
+   hash: {
+     name: "Hash",
+     url: "https://hash.ai",
+     description: "Lorem ipsum dolor sit amet etc etc",
+     demo: "https://core.hash.ai/new",
+   },
 
-  // great_expectations: {
-  //   demo: "https://docs.greatexpectations.io/en/latest/guides/tutorials/quick_start.html#tutorials-quick-start",
-  // },
+   superconductive: {
+     name: "Superconductive",
+     url: "https://www.superconductive.ai",
+     description: "Lorem ipsum dolor sit amet etc etc",
+     demo: "https://docs.greatexpectations.io/en/latest/guides/tutorials/quick_start.html#tutorials-quick-start",
+   },
 
-  // okteto: {
-  //   demo: "https://okteto.com/docs/getting-started/index.html",
-  // },
+   okteto: {
+     name: "Okteto",
+     url: "https://okteto.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+     demo: "https://okteto.com/docs/getting-started/index.html",
+   },
 
-  // privacy_dynamics: {
-  //   demo: "https://www.privacydynamics.io/demo/contact",
-  // },
+   privacy_dynamics: {
+     name: "Privacy Dynamics",
+     url: "https://privacydynamics.io",
+     description: "Lorem ipsum dolor sit amet etc etc",
+     demo: "https://www.privacydynamics.io/demo/contact",
+   },
+  
+   nautilus_labs: {
+     name: "Nautilus Labs",
+     url: "https://nautiluslabs.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+ 
 };
