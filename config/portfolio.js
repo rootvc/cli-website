c5ff38b48e9d5d5b69d8297973a1797bca6bf387
emilyhenriksson <79349355+emilyhenriksson@users.noreply.github.com>
2021-06-03 11:19:48 -0700

    Update portfolio.js
    Added more cos

--- config/portfolio.js
+++ config/portfolio.js
@@ -74,5 +74,181 @@ const portfolio = {
      url: "https://www.tortugaagtech.com",
      description: "Lorem ipsum dolor sit amet etc etc",
    },
+  
+   instrumental: {
+     name: "Instrumental",
+     url: "https://instrumental.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   serve: {
+     name: "Serve",
+     url: "https://serveautomation.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   versatile: {
+     name: "Versatile",
+     url: "https://www.versatile.ai",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   dusty_robotics: {
+     name: "Dusty Robotics",
+     url: "https://www.dustyrobotics.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   thruwave: {
+     name: "ThruWave",
+     url: "https://www.thruwave.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   seismic: {
+     name: "Seismic",
+     url: "https://www.myseismic.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   seam: {
+     name: "Seam",
+     url: "https://getseam.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   chargelab: {
+     name: "ChargeLab",
+     url: "https://www.chargelab.co",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   wildtype: {
+     name: "Wildtype",
+     url: "https://www.wildtypefoods.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   nordsense: {
+     name: "Nordsense",
+     url: "https://nordsense.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   trucklabs: {
+     name: "TruckLabs",
+     url: "https://www.trucklabs.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   coral: {
+     name: "Coral",
+     url: "https://www.trycoral.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   thread: {
+     name: "Thread",
+     url: "https://threadfertility.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   sensable: {
+     name: "Sensable",
+     url: "https://www.getsensable.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   crux_ocm: {
+     name: "Crux OCM",
+     url: "https://www.cruxocm.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
  
+   alan: {
+     name: "The Alan Programming Language",
+     url: "https://alan-lang.org",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   mashgin: {
+     name: "Mashgin",
+     url: "https://www.mashgin.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   cloudastructure: {
+     name: "Cloudastructure",
+     url: "https://cloudastructure.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   creator: {
+     name: "Creator",
+     url: "https://www.creator.rest",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   apolloshield: {
+     name: "ApolloShield",
+     url: "https://www.apolloshield.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   skycatch: {
+     name: "Skycatch",
+     url: "https://skycatch.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   shaper: {
+     name: "Shaper",
+     url: "https://www.shapertools.com/en-us",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   cape: {
+     name: "Cape",
+     url: "https://capenetworks.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   loom_vision: {
+     name: "Loom",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   plethora: {
+     name: "Plethora",
+     url: "https://www.plethora.com",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   preact: {
+     name: "Preact",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   prynt: {
+     name: "Prynt",
+     url: "https://www.prynt.co/#/?_k=4hdttp",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   righthook: {
+     name: "RightHook",
+     url: "https://righthook.io",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   seecontrol: {
+     name: "SeeControl",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
+   wallaby: {
+     name: "Wallaby",
+     description: "Lorem ipsum dolor sit amet etc etc",
+   },
+  
 };
