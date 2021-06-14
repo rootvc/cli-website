4280d8932879ed2a9d0e175aa6da8283c9ba9ee4
Kane Hsieh <peepeepoo@computah.local>
2021-06-14 16:45:01 -0700

    tab space the tldr command

--- config/commands.js
+++ config/commands.js
@@ -56,8 +56,9 @@ const commands = {
         const data = portfolio[c];
         const tabs = `\t\t`;
         term.stylePrint(c);
-        if (c.length > 11)
+        if (${c}.length > 11) {
           const tabs = `\t`;
+        }
         term.stylePrint(`%tldr% ${c}`+tabs+`${colorText("or", "user")} visit: ${data["url"]}`);
       }
     } else if (!portfolio[name]) {
