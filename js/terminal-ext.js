f4779c0949baffe822ad3b2643ca8c43cd024ce1
P2501 <P2501@P2501.local>
2022-11-16 18:13:48 -0800

    new copy

--- js/terminal-ext.js
+++ js/terminal-ext.js
@@ -172,7 +172,7 @@ extend = (term) => {
     term.printLogoType();
     if (term.VERSION == 2) {
       term.stylePrint(`\n${colorText("New version of Root Ventures detected.", "user")}`);
-      term.stylePrint(`Please upgrade your terminal with ${colorText("upgrade", "command")}.`);
+      term.stylePrint(`Please upgrade your terminal with ${colorText("upgrade", "command")}. Type ${colorText("help", "command")} to get started. Or type ${colorText("exit", "command")} for web version.`);
     } else {
       term.stylePrint('Welcome to the Root Ventures terminal. Seeding bold engineers!');
       term.stylePrint(`Type ${colorText("help", "command")} to get started. Or type ${colorText("exit", "command")} for web version.`, false);
