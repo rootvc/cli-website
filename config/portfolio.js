e34210f1dc00174d7d17502dc62b5cdc0b944ae2
Yasyf Mohamedali <yasyfm@gmail.com>
2022-12-14 15:07:46 -0800

    Add Ruby

--- config/portfolio.js
+++ config/portfolio.js
@@ -2,7 +2,8 @@ const portfolio = {
   esper: {
     name: "Esper",
     url: "https://esper.io",
-    description: "Esper is devops for devices, allowing teams to seamlessly build and manage enterprise hardware.",
+    description:
+      "Esper is devops for devices, allowing teams to seamlessly build and manage enterprise hardware.",
     memo: null,
     demo: "https://esper.io/signup",
   },
@@ -10,7 +11,8 @@ const portfolio = {
   meroxa: {
     name: "Meroxa",
     url: "https://meroxa.com",
-    description: "Meroxa is a platform for software engineers to build mature, scalable data engineering infrastructure with a single command.",
+    description:
+      "Meroxa is a platform for software engineers to build mature, scalable data engineering infrastructure with a single command.",
     memo: "https://github.com/rootvc/investment-memos/blob/main/meroxa.md",
     demo: "https://meroxa.com/#data-warehouse",
   },
@@ -18,28 +20,32 @@ const portfolio = {
   particle: {
     name: "Particle",
     url: "https://particle.io",
-    description: "Particle is the largest professional IoT development platform.",
+    description:
+      "Particle is the largest professional IoT development platform.",
     demo: "https://docs.particle.io/",
   },
 
   daily: {
     name: "Daily",
     url: "https://daily.co",
-    description: "Daily makes it easier for developers to add video to websites and apps.",
+    description:
+      "Daily makes it easier for developers to add video to websites and apps.",
     demo: "https://docs.daily.co/docs/introduction-1",
   },
 
   hash: {
     name: "Hash",
     url: "https://hash.ai",
-    description: "Hash is a web-based platform for simulation and modeling, starting with agent-based modeling (ABM) and simulation.",
+    description:
+      "Hash is a web-based platform for simulation and modeling, starting with agent-based modeling (ABM) and simulation.",
     demo: "https://core.hash.ai/new",
   },
 
   superconductive: {
     name: "Superconductive",
     url: "https://superconductive.ai",
-    description: "Superconductive is a SaaS platform for verifying and enforcing data integrity at every stage of the lifecycle of data in an organization.",
+    description:
+      "Superconductive is a SaaS platform for verifying and enforcing data integrity at every stage of the lifecycle of data in an organization.",
     demo: "https://docs.greatexpectations.io/en/latest/guides/tutorials/quick_start.html#tutorials-quick-start",
   },
 
@@ -53,14 +59,16 @@ const portfolio = {
   privacy_dynamics: {
     name: "Privacy Dynamics",
     url: "https://privacydynamics.io",
-    description: "Privacy Dynamics ensures your customers' data privacy without slowing down your team.",
+    description:
+      "Privacy Dynamics ensures your customers' data privacy without slowing down your team.",
     demo: "https://privacydynamics.io/demo/contact",
   },
 
   nautilus: {
     name: "Nautilus Labs",
     url: "https://nautiluslabs.com",
-    description: "Nautilus Labs allows maritime fleets to optimize shipping routes and energy costs in real time.",
+    description:
+      "Nautilus Labs allows maritime fleets to optimize shipping routes and energy costs in real time.",
   },
 
   ntopology: {
@@ -78,37 +86,43 @@ const portfolio = {
   instrumental: {
     name: "Instrumental",
     url: "https://instrumental.com",
-    description: "Instrumental generates real time insights for mass manufacturing.",
+    description:
+      "Instrumental generates real time insights for mass manufacturing.",
   },
 
   stellar: {
     name: "Stellar Pizza",
     url: "https://eatstellarpizza.com",
-    description: "Stellar Pizza is building the future of automated food production.",
+    description:
+      "Stellar Pizza is building the future of automated food production.",
   },
 
   versatile: {
     name: "Versatile",
     url: "https://versatile.ai",
-    description: "Versatile is an onsite construction data provider for accelerating schedules.",
+    description:
+      "Versatile is an onsite construction data provider for accelerating schedules.",
   },
 
   dusty: {
     name: "Dusty Robotics",
     url: "https://dustyrobotics.com",
-    description: "Dusty Robotics automates layout in complex construction projects.",
+    description:
+      "Dusty Robotics automates layout in complex construction projects.",
   },
 
   thruwave: {
     name: "ThruWave",
     url: "https://thruwave.com",
-    description: "Thruwave increases the efficiency and transparency of high volume logistics operations.",
+    description:
+      "Thruwave increases the efficiency and transparency of high volume logistics operations.",
   },
 
   seismic: {
     name: "Seismic",
     url: "https://myseismic.com",
-    description: "Seismic augments strength and safety for workers through soft robotics.",
+    description:
+      "Seismic augments strength and safety for workers through soft robotics.",
   },
 
   seam: {
@@ -120,25 +134,29 @@ const portfolio = {
   chargelab: {
     name: "ChargeLab",
     url: "https://chargelab.co",
-    description: "ChargeLab builds API- and software-defined EV charging networks.",
+    description:
+      "ChargeLab builds API- and software-defined EV charging networks.",
   },
 
   wildtype: {
     name: "Wildtype Foods",
     url: "https://wildtypefoods.com",
-    description: "Wildtype creates organic, clean, high-quality, animal-free meat.",
+    description:
+      "Wildtype creates organic, clean, high-quality, animal-free meat.",
   },
 
   nordsense: {
     name: "Nordsense",
     url: "https://nordsense.com",
-    description: "Nordsense optimizes pickup routes for waste management fleets.",
+    description:
+      "Nordsense optimizes pickup routes for waste management fleets.",
   },
 
   trucklabs: {
     name: "TruckLabs",
     url: "https://trucklabs.com",
-    description: "Trucklabs reduces fuel costs and increases profit margins for long haul trucking fleets.",
+    description:
+      "Trucklabs reduces fuel costs and increases profit margins for long haul trucking fleets.",
   },
 
   oma_fertility: {
@@ -150,7 +168,8 @@ const portfolio = {
   sensable: {
     name: "Sensable",
     url: "https://getsensable.com",
-    description: "Sensable generates real-time industrial engineering insights.",
+    description:
+      "Sensable generates real-time industrial engineering insights.",
   },
 
   crux: {
@@ -162,8 +181,9 @@ const portfolio = {
   iasql: {
     name: "IaSQL",
     url: "https://iasql.com",
-    description: "IaSQL is the next evolution of infrastructure management, allowing you to manage your infra in a database instead of stateless config files.",
-    demo: "https://docs.iasql.com/"
+    description:
+      "IaSQL is the next evolution of infrastructure management, allowing you to manage your infra in a database instead of stateless config files.",
+    demo: "https://docs.iasql.com/",
   },
 
   mashgin: {
@@ -181,63 +201,72 @@ const portfolio = {
   apolloshield: {
     name: "ApolloShield",
     url: "https://apolloshield.com",
-    description: "ApolloShield safeguards airspace from autonomous and remote piloted aircraft.",
+    description:
+      "ApolloShield safeguards airspace from autonomous and remote piloted aircraft.",
   },
 
   skycatch: {
     name: "Skycatch",
     url: "https://skycatch.com",
-    description: "Skycatch provides high resolution 3D mapping and modeling of high-value infrastructure.",
+    description:
+      "Skycatch provides high resolution 3D mapping and modeling of high-value infrastructure.",
   },
 
   shaper: {
     name: "Shaper",
     url: "https://shapertools.com",
-    description: "Shaper Tools makes the Origin, a handheld, auto-correcting CNC.",
+    description:
+      "Shaper Tools makes the Origin, a handheld, auto-correcting CNC.",
   },
 
   cape: {
     name: "Cape",
     url: "https://capenetworks.com",
-    description: "Cape Networks allows IT professionals to monitor, text, and repair enterprise networks remotely.",
+    description:
+      "Cape Networks allows IT professionals to monitor, text, and repair enterprise networks remotely.",
   },
 
   righthook: {
     name: "Righthook",
     url: "https://righthook.io",
-    description: "Righthook reduces the time and cost required to develop autonomous vehicles.",
+    description:
+      "Righthook reduces the time and cost required to develop autonomous vehicles.",
   },
 
   sixwheel: {
     name: "SixWheel",
     url: "https://sixwheel.com",
-    description: "SixWheel brings autonomy and electrification to long haul trucking operations.",
+    description:
+      "SixWheel brings autonomy and electrification to long haul trucking operations.",
   },
 
   radical: {
     name: "Radical",
     url: "https://radicalsemiconductor.com",
-    description: "Radical Semiconductor provides hardware security down to the individual IC.",
+    description:
+      "Radical Semiconductor provides hardware security down to the individual IC.",
   },
 
   zed: {
     name: "Zed",
     url: "https://zed.dev",
-    description: "Zed is a fully-native desktop code editor focused on high performance, clean design, and real-time collaboration.",
+    description:
+      "Zed is a fully-native desktop code editor focused on high performance, clean design, and real-time collaboration.",
   },
 
   kayhan: {
     name: "Kayhan",
     url: "https://kayhan.space",
-    description: "Kayhan’s spaceflight operations platform allows satellite operators to focus on their core mission.",
+    description:
+      "Kayhan’s spaceflight operations platform allows satellite operators to focus on their core mission.",
   },
-  
+
   allspice: {
     name: "AllSpice",
     url: "https://allspice.io",
     description: "Collaboration and testing platform for hardware teams.",
   },
-  
+
   quilter: {
     name: "Quilter",
     url: "https://quilter.ai",
@@ -249,35 +278,44 @@ const portfolio = {
     url: "https://adept.ai",
     description: "Useful general intelligence.",
   },
-  
+
   aperture: {
     name: "Aperture Data",
     url: "https://aperturedata.io",
-    description: "Aperture manage images and videos with a database purpose-built for data science and machine learning."
+    description:
+      "Aperture manage images and videos with a database purpose-built for data science and machine learning.",
   },
-  
+
   supertokens: {
     name: "SuperTokens",
     url: "https://supertokens.io",
-    description: "SuperTokens is the best Open Source solution for user authentication."
+    description:
+      "SuperTokens is the best Open Source solution for user authentication.",
   },
 
   fudge: {
     name: "Fudge",
     url: "https://fudge.ai",
-    description: "Fudge is the best way to speed up your website to improve conversion rates and increase revenue."
+    description:
+      "Fudge is the best way to speed up your website to improve conversion rates and increase revenue.",
   },
 
   kodra: {
     name: "Kodra",
     url: "https://kodra.ai",
-    description: "Fast dataset curation for machine learning."
+    description: "Fast dataset curation for machine learning.",
   },
 
   topologic: {
     name: "Topologic",
     url: "https://topologic.io",
-    description: "Design and automation software for textile manufacturing."
-  }
-  
+    description: "Design and automation software for textile manufacturing.",
+  },
+
+  ruby: {
+    name: "Ruby Robotics",
+    url: "https://ruby-robotics.com",
+    description:
+      "Vertically integrated robotics and AI to autonomously prepare, image and assess tissue during biopsy procedures.",
+  },
 };
