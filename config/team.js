9801d1615159b034e165c2cfbcaf1e49073b6c78
Yasyf Mohamedali <yasyfm@gmail.com>
2023-04-17 20:42:48 -0700

    Add Yasyf

--- config/team.js
+++ config/team.js
@@ -1,38 +1,51 @@
 const team = {
-  "avidan": {
+  avidan: {
     name: "Avidan Ross",
     title: "Managing Partner",
-    description: "Avidan is the Founding Partner of Root Ventures. Previously, he designed industrial robotics for Food Network's kitchens and was CTO of CIM Group, where he focused on industrial investing, and worked as an embedded application developer at Excite@Home. Avidan has a BA in Computer Science from Columbia University.",
+    description:
+      "Avidan is the Founding Partner of Root Ventures. Previously, he designed industrial robotics for Food Network's kitchens and was CTO of CIM Group, where he focused on industrial investing, and worked as an embedded application developer at Excite@Home. Avidan has a BA in Computer Science from Columbia University.",
     linkedin: "https://www.linkedin.com/in/avidanross/",
   },
-  "kane": {
+  kane: {
     name: "Kane Hsieh",
     title: "Partner",
-    description: "Before joining Root Ventures, Kane was founder and Head of Product at Brilliant Bicycle Co. He has also worked as an early-stage investor at RRE Ventures, a software engineer at Romotive, and a Project Manager at Microsoft. Kane has an AB in Computer Science from Harvard.",
+    description:
+      "Before joining Root Ventures, Kane was founder and Head of Product at Brilliant Bicycle Co. He has also worked as an early-stage investor at RRE Ventures, a software engineer at Romotive, and a Project Manager at Microsoft. Kane has an AB in Computer Science from Harvard.",
     linkedin: "https://www.linkedin.com/in/kanehsieh/",
   },
-  "chrissy": {
+  chrissy: {
     name: "Chrissy Meyer",
     title: "Partner",
-    description: "Chrissy has spent the past decade developing and shipping hardware as an engineering manager at Apple and Square. She was a founding team member at Pearl Automation, a vehicle technology startup. Chrissy has an MS in Electrical Engineering from Stanford and a BSEE from Rose-Hulman.",
+    description:
+      "Chrissy has spent the past decade developing and shipping hardware as an engineering manager at Apple and Square. She was a founding team member at Pearl Automation, a vehicle technology startup. Chrissy has an MS in Electrical Engineering from Stanford and a BSEE from Rose-Hulman.",
     linkedin: "https://www.linkedin.com/in/chrissymeyer/",
   },
-  "lee": {
+  lee: {
     name: "Lee Edwards",
     title: "Partner",
-    description: "Lee was most recently CTO at Teespring. Previously, Lee was a mechanical engineer at iRobot, a software engineer at Pivotal Labs, Lead Engineer at SideTour (acquired by Groupon in 2013), and engineering manager for GrouponLive. He graduated from Olin College of Engineering with a degree in Systems Engineering.",
+    description:
+      "Lee was most recently CTO at Teespring. Previously, Lee was a mechanical engineer at iRobot, a software engineer at Pivotal Labs, Lead Engineer at SideTour (acquired by Groupon in 2013), and engineering manager for GrouponLive. He graduated from Olin College of Engineering with a degree in Systems Engineering.",
     linkedin: "https://www.linkedin.com/in/leeredwards/",
   },
-  "emily": {
+  emily: {
     name: "Emily Henriksson",
     title: "Principal",
-    description: "Prior to Root, Emily designed flight hardware for the SpaceX Falcon and supervised vehicle build for schedule-critical missions. She also worked on Model 3 battery module prototyping at Tesla. Emily holds MS and BS degrees in Mechanical Engineering from Stanford where she was a captain for the Division I Women’s Field Hockey Team. She also holds an MBA from Harvard Business School.",
+    description:
+      "Prior to Root, Emily designed flight hardware for the SpaceX Falcon and supervised vehicle build for schedule-critical missions. She also worked on Model 3 battery module prototyping at Tesla. Emily holds MS and BS degrees in Mechanical Engineering from Stanford where she was a captain for the Division I Women’s Field Hockey Team. She also holds an MBA from Harvard Business School.",
     linkedin: "https://www.linkedin.com/in/emily-henriksson-42959737/",
   },
-  "laelah": {
+  yasyf: {
+    name: "Yasyf Mohamedali",
+    title: "E*IR",
+    description:
+      "Yasyf := (Engineer|Entrepreneur|Extra Helpful Human)-in-Residence.\n\rYasyf ∈\n\r\tFounders_{exited} &\n\r\tAlum_{MIT} &\n\r\tAngel_{devtools,infra,ML,healthcare}\n\r.\n\rHobbies_{Yasyf} := (food|travel|hosting|hacking).",
+    linkedin: "https://www.linkedin.com/in/yasyf",
+  },
+  laelah: {
     name: "Laelah Reino",
     title: "Operations Manager",
-    description: "Laelah has spent over 15 years in marketing for films, consumer products, and subscription-based services. Laelah Reino has a BA in Business Administration with a Marketing concentration from Drexel University.",
+    description:
+      "Laelah has spent over 15 years in marketing for films, consumer products, and subscription-based services. Laelah Reino has a BA in Business Administration with a Marketing concentration from Drexel University.",
     linkedin: "https://www.linkedin.com/in/laelah-reino-78b6a51/",
   },
 };
