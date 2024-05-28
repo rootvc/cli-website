e0a0562c44a122fc90936df07a49feeef0db1dec
Avidan Ross <avidan@root.vc>
2024-05-28 16:36:47 -0700

    Update jobs.js
    the office upgrades (1,2,3) had no url
    added engineering experience as an option

--- config/jobs.js
+++ config/jobs.js
@@ -5,7 +5,7 @@ const jobs = {
     "We're hiring an Investor!",
 
     "\r\nRoot Ventures is a $325M Seed VC based in San Francisco. We invest in technical teams at the earliest stages, focusing on founders building products at the frontier of technology and founders building the tools to make it all possible.",
-    "\r\nYour role will include identifying & analyzing potential investment opportunities, assisting with due diligence, performing market research & competitive analysis, and strengthening our network through hosting & attending events. We're a team of engineers and tinkerers, so you'll even be helping us with our constant office improvement projects (1, 2, 3).",
+    "\r\nYour role will include identifying & analyzing potential investment opportunities, assisting with due diligence, performing market research & competitive analysis, and strengthening our network through hosting & attending events. We're a team of engineers and tinkerers, so you'll even be helping us with our constant office improvement projects.",
 
     "\r\nAbout the job:",
     "We operate at the intersection between hard technologies & business. We obsess about the latest technological advancements, and how they will change entire industries and humanity. This is not an engineering job. This is not a strategy consulting job. It’s definitely not an investment banking job. We work in a strange place in between. As a member of the Root investment team, you’ll get to spend time thinking about technical challenges and their industry implications at very deep levels",
@@ -20,7 +20,7 @@ const jobs = {
 
     "\r\nJob qualifications:",
     " * Strong communication and analytical skills.",
-    " * At least 3-5 years of work experience in VC, startup operations or finance.",
+    " * At least 3-5 years of work experience in VC, startup operations, engineering, or finance.",
     " * Based or able to relocate to the San Francisco Bay Area. We are an in-office team.",
   ],
 };
