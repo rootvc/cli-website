cdd8422fb67afe690910a1b98f27420e1c229678
Kane Hsieh <multivac@multivac.local>
2024-12-10 18:06:35 -0800

    copy

--- js/terminal-ext.js
+++ js/terminal-ext.js
@@ -10,30 +10,33 @@ extend = (term) => {
   term.history = [];
   term.historyCursor = -1;
   term.pos = () => term._core.buffer.x - term._promptRawText().length - 1;
-  term._promptRawText = () => `${term.user}${term.sep}${term.host} ${term.cwd} $`;
+  term._promptRawText = () =>
+    `${term.user}${term.sep}${term.host} ${term.cwd} $`;
   term.deepLink = window.location.hash.replace("#", "").split("-").join(" ");
 
   term.promptText = () => {
-    var text = term._promptRawText().replace(term.user, colorText(term.user, "user"))
+    var text = term
+      ._promptRawText()
+      .replace(term.user, colorText(term.user, "user"))
       .replace(term.sep, colorText(term.sep, ""))
       .replace(term.host, colorText(term.host, ""))
       .replace(term.cwd, colorText(term.cwd, "hyperlink"))
       .replace(term._promptChar, colorText(term._promptChar, "prompt"));
     return text;
-  }
+  };
 
-  term.timer = ms => new Promise(res => setTimeout(res, ms));
+  term.timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
   term.dottedPrint = async (phrase, n, newline = true) => {
     term.write(phrase);
 
     for (let i = 0; i < n; i++) {
-      await term.delayPrint('.', 1000);
+      await term.delayPrint(".", 1000);
     }
     if (newline) {
       term.write("\r\n");
     }
-  }
+  };
 
   term.progressBar = async (t, msg) => {
     var r;
@@ -44,29 +47,29 @@ extend = (term) => {
     term.write("\r\n[");
 
     for (let i = 0; i < term.cols / 2; i = i + 1) {
-      r = Math.round(Math.random() * t / 20);
+      r = Math.round((Math.random() * t) / 20);
       t = t - r;
-      await term.delayPrint('█', r);
+      await term.delayPrint("█", r);
     }
     term.write("]\r\n");
-  }
+  };
 
   term.delayPrint = async (str, t) => {
     await term.timer(t);
     term.write(str);
-  }
+  };
 
   term.delayStylePrint = async (str, t, wrap) => {
     await term.timer(t);
     term.stylePrint(str, wrap);
-  }
+  };
 
   term.prompt = (prefix = "\r\n", suffix = " ") => {
     term.write(`${prefix}${term.promptText()}${suffix}`);
   };
 
   term.clearCurrentLine = (goToEndofHistory = false) => {
-    term.write('\x1b[2K\r');
+    term.write("\x1b[2K\r");
     term.prompt("", " ");
     term.currentLine = "";
     if (goToEndofHistory) {
@@ -81,13 +84,14 @@ extend = (term) => {
     term.currentLine = newLine;
     term.write(newLine);
     if (preserveCursor) {
-      term.write('\x1b[D'.repeat(length - term.pos()));
+      term.write("\x1b[D".repeat(length - term.pos()));
     }
-  }
+  };
 
   term.stylePrint = (text, wrap = true) => {
     // Hyperlinks
-    const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const urlRegex =
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
     const urlMatches = text.matchAll(urlRegex);
     let allowWrapping = true;
     for (match of urlMatches) {
@@ -116,11 +120,11 @@ extend = (term) => {
     if (term.cols >= 40) {
       term.writeln(`\r\n${getArt(id)}\r\n`);
     }
-  }
+  };
 
   term.printLogoType = () => {
     term.writeln(term.cols >= 40 ? LOGO_TYPE : "[Root Ventures]\r\n");
-  }
+  };
 
   term.openURL = (url, newWindow = true) => {
     term.stylePrint(`Opening ${url}`);
@@ -132,23 +136,23 @@ extend = (term) => {
         window.location.href = url;
       }
     }
-  }
+  };
 
   term.displayURL = (url) => {
     term.stylePrint(colorText(url, "hyperlink"));
-  }
+  };
 
   term.command = (line) => {
     const parts = line.split(/\s+/);
     const cmd = parts[0].toLowerCase();
-    const args = parts.slice(1, parts.length)
+    const args = parts.slice(1, parts.length);
     const fn = commands[cmd];
-    if (typeof (fn) === "undefined") {
+    if (typeof fn === "undefined") {
       term.stylePrint(`Command not found: ${cmd}. Try 'help' to get started.`);
     } else {
       return fn(args);
     }
-  }
+  };
 
   term.resizeListener = () => {
     term._initialized = false;
@@ -169,9 +173,17 @@ extend = (term) => {
     preloadFiles();
     term.reset();
     term.printLogoType();
-    term.stylePrint('Welcome to the Root Ventures terminal. Seeding bold engineers!');
-    term.stylePrint(`Type ${colorText("help", "command")} to get started. Or type ${colorText("exit", "command")} for web version.`, false);
-    term.stylePrint(`\r\nOpen jobs detected. Type ${colorText("jobs", "command")} for more info.`, false);
+    term.stylePrint(
+      "Welcome to the Root Ventures terminal. We are investing in technical founders.",
+    );
+    term.stylePrint(
+      `Type ${colorText("help", "command")} to get started. Or type ${colorText("exit", "command")} for web version.`,
+      false,
+    );
+    term.stylePrint(
+      `\r\nOpen jobs detected. Type ${colorText("jobs", "command")} for more info.`,
+      false,
+    );
 
     term.user = user;
     if (!preserveHistory) {
@@ -184,19 +196,21 @@ extend = (term) => {
     if (term.deepLink != "") {
       term.command(term.deepLink);
     }
-  }
-}
+  };
+};
 
 // https://stackoverflow.com/questions/14484787/wrap-text-in-javascript
 // TODO: This doesn't work well at detecting newline
 function _wordWrap(str, maxWidth) {
-  var newLineStr = "\r\n"; done = false; res = '';
+  var newLineStr = "\r\n";
+  done = false;
+  res = "";
   while (str.length > maxWidth) {
     found = false;
     // Inserts new line at first whitespace of the line
     for (i = maxWidth - 1; i >= 0; i--) {
       if (_testWhite(str.charAt(i))) {
-        res = res + [str.slice(0, i), newLineStr].join('');
+        res = res + [str.slice(0, i), newLineStr].join("");
         str = str.slice(i + 1);
         found = true;
         break;
@@ -204,7 +218,7 @@ function _wordWrap(str, maxWidth) {
     }
     // Inserts new line at maxWidth position, the word is too long to wrap
     if (!found) {
-      res += [str.slice(0, maxWidth), newLineStr].join('');
+      res += [str.slice(0, maxWidth), newLineStr].join("");
       str = str.slice(maxWidth);
     }
   }
@@ -214,4 +228,4 @@ function _wordWrap(str, maxWidth) {
 function _testWhite(x) {
   var white = new RegExp(/^\s$/);
   return white.test(x.charAt(0));
-};
+}
